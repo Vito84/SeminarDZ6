c5e22165197ebf03680992b97ee4002f14d353e9
ViktorAnikin <Avka3d@mail.ru>
2023-01-25 19:38:31 +0500

    Add SeminarDZ6

---
+++ DZ41/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+ 
+Console.WriteLine("Введите цифры через пробел");
+string numbers = Console.ReadLine() ?? "NULL";
+int[] arrNumbers = numbers.Split(' ').Select(Int32.Parse).ToArray();
+int sum = 0;
+for (int i = 0; i < arrNumbers.Length; i++)
+{
+    if (arrNumbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0 {sum}");
---
+++ DZ43/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+ 
+    while (true)
+    {
+        Console.WriteLine(message);
+ 
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("ВЫ ввели не число.");
+            Console.WriteLine("__________________");
+        }
+    }
+ 
+    return result;
+}
+void findingXy (double b1, double b2, double k1, double k2)
+{
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+Console.WriteLine($"Точка пересечения двух прямых = ({x}; {y})");
